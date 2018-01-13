0d2be2b8c68529935ca8a7db54a8dd6bf7b9df88
zeochoy <zeochoy@gmail.com>
2018-01-14 03:00:15 +0800

    minor fix

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='resim',
-    version='0.1.0',
+    version='0.1.1',
     description='chemoREsistance SIMulator',
     long_description=long_description,
     keywords='chemoresistance cancer outcome simulations modeling',
